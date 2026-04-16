d8b4369e6687d04f332f1527cd22eee2a372458b
Jakub Kowalczyk <jakub.kowalczyk@stxnext.pl>
2026-04-16 12:38:26 +0200

    feat: initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
-import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
-import { useI18n } from 'vue-i18n'
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { useI18n } from "vue-i18n";
 
-const { t } = useI18n()
-
+const { t } = useI18n();
 </script>
 
 <template>
-  <div class="flex items-center justify-center min-h-screen p-10">
+  <div
+    class="flex items-center justify-center min-h-screen p-10 bg-fuchsia-800"
+  >
     <Card class="w-87.5">
       <CardHeader>
         <CardTitle>Keeping It Sane!</CardTitle>
       </CardHeader>
       <CardContent>
-        <p>{{ t('welcome') }}</p>
-        <Button class="mt-4">{{ t('login') }}</Button>
+        <p>{{ t("welcome") }}</p>
+        <div class="flex items-center justify-end">
+          <Button class="mt-4">{{ t("login") }}</Button>
+        </div>
       </CardContent>
     </Card>
   </div>
